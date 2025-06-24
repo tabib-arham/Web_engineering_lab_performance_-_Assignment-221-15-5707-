7eef8595c15523f28b13055e813b246cce9b463a
tabib-arham <140184089+tabib-arham@users.noreply.github.com>
2025-06-24 12:33:12 +0600

    Add files via upload

---
+++ web_lab_4_performance_221-15-5707/script.js
@@ -0,0 +1,51 @@
+// // Declare variables
+// var name = "tabib";
+// console.log(name);
+
+// let roll = 29;
+// console.log(roll);
+
+// // Block scope variable - no issues
+// {
+//     let a = 12;
+// }
+
+// // Object declaration - use 'let' or 'const'
+// let Student = {
+//     name: "tabib",
+//     roll: 29,
+//     section: "61_k1"
+// };
+
+// console.log(Student.name);
+// console.log(Student["roll"]);
+
+// // Conditionals
+// let a = 12;
+// if (a % 2 === 0) {
+//     console.log("a is even");
+// } else {
+//     console.log("a is odd");
+// }
+
+// let ab = 12;
+// if (ab === 12) {
+//     console.log("a is same");
+// } else {
+//     console.log("a is not same"); // typo fixed: "seme" → "same"
+// }
+
+
+
+// // Arrays and loop
+// let arr = [];
+// arr.push(12);
+// arr.push("tabib");
+// console.log(arr);
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// //DOM manupulation
+// //
